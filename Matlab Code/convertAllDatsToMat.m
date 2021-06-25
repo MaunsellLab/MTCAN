82b5ec706a69d28e9358d2f1bfd8a76dffdf0268
cherianchery <cherianchery@uchicaog.edu>
2021-06-25 15:57:14 -0500

    script to convert unconverted .dat files to .mat files

--- Matlab Code/convertAllDatsToMat.m
+++ Matlab Code/convertAllDatsToMat.m
@@ -4,8 +4,10 @@ function convertAllDatsToMat()
 % to .mat file and save it in Matlab Data folder
 
 myDir = uigetdir; % sets search dir
-myFiles = dir(fullfile(myDir, '*.dat')); % gets all .dat files into struct
+myFiles = dir(fullfile(myDir, '*.dat')); 
 
+% scans through files in LLDir, and converts
+% them if they're not converted
 for k = 1:length(myFiles)
   fileName = erase(myFiles(k).name, '.dat');
   fileNameMat = append(fileName, '.mat');

eee9cbfbe453de19cdadea7d15b90da4c6e34b54
cherianchery <cherianchery@uchicaog.edu>
2021-07-27 19:22:39 -0500

    Create stimSequenceCorroboration.m
    mindmap of checking stim sequence script

---
+++ Matlab Code/stimSequenceCorroboration.m
@@ -0,0 +1,8 @@
+
+% create a list of the first trial sequence
+% go through second trial sequence - check to see if sequence is same
+%   - if sequence is not same then return error message
+%   - if second sequence is greater length, add extra stims to first
+%   sequence list
+% go through rest of the trials
+% if trials not the sequence then return error message

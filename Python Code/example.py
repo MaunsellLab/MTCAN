f460fed5c95f8f7d46508f01bf0090348d66e2eb
cherianchery <cherianchery@uchicaog.edu>
2021-09-11 19:42:32 -0500

    using example.py as a backup

--- Python Code/example.py
+++ Python Code/example.py
@@ -94,3 +94,61 @@ for i in range(0,len(allTrialsData.item()[0])-1):
                             if indexCurrStim[0][0] != indexLastStim[0][0] + 1:
                                 print('not startsequence not in correct order')
                         lastStimSeq = currStimSeq
+                        
+# Attempt 2
+
+import scipy.io as sp
+import numpy as np
+
+#importing mat file with multiple trials
+allTrials = sp.loadmat('xxx.mat', squeeze_me = True)
+allTrialsData = allTrials['trials']
+
+locLsts = [[],[],[],[]] 
+lastStim = [[],[],[],[]] 
+currStim = [[],[],[],[]]
+
+#to run through diff trials 
+for i in range(0,len(allTrialsData.item()[0])-1):
+    print(i)
+    currTrial = allTrialsData.item()[0][i]
+    trial = currTrial['trial']
+    trialEnd = currTrial['trialEnd']
+
+    if trialEnd.item()['data'] == 0: 
+        if trial.item()['data'].item()['instructTrial'] == 0:
+            stimDesc = currTrial['stimDesc']
+            for count, d in enumerate(stimDesc.item()['data'].item()['listTypes']):
+                if 2 in d:
+                    targetOnsetStim = count
+            attendLoc = trial.item()['data'].item()['attendLoc'].tolist()
+            stimSeqCheck(attendLoc)
+
+def stimSeqCheck(attendLoc):
+    '''
+    Function to create a stimulus seq based on attended location.
+    Function will also ensure that subsequence trials follow the stimulus
+    sequence that was generated. 
+
+    inputs: attendedLoc (integer)
+    outputs: appends to locLsts (list of lists), where 0,1,2,3 describe 
+             attended loc
+             print statement if something is out of sequence
+    '''
+    for d in stimDesc.item()['data'].item()['stimTypes'][1:targetOnsetStim]:
+        if len(locLsts[attendLoc]) < 9:
+            locLsts[attendLoc].append(d[2:4].tolist())
+            lastStim[attendLoc] = d[2:4].tolist()
+        else:
+            indexLastStim = [(count,i) for count, i in enumerate(locLsts[attendLoc]) \
+                if i == lastStim[attendLoc]]
+            currStim[attendLoc] = d[2:4].tolist()
+            indexCurrStim = [(count,i) for count, i in enumerate(locLsts[attendLoc]) \
+                if i == currStim[attendLoc]]
+            if indexLastStim[0][0] == len(locLsts[attendLoc]) - 1:
+                if indexCurrStim[0][0] != 0:
+                    print('start sequence not in correct order')
+            else:
+                if indexCurrStim[0][0] != indexLastStim[0][0] + 1:
+                    print('middle sequence not in correct order')
+            lastStim[attendLoc] = currStim[attendLoc]

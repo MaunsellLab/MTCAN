cf65bd0d4e7c1c21b835f89a6e42b40684fac284
cherianchery <cherianchery@uchicaog.edu>
2021-09-12 22:52:56 -0500

    Update stimSequenceCheck.py

--- Python Code/stimSequenceCheck.py
+++ Python Code/stimSequenceCheck.py
@@ -1,7 +1,9 @@
 import scipy.io as sp
 import numpy as np
 
-# Import .mat file with multiple trials
+# when importing .mat file, make sure it is the combined file with both the 
+# header and all trials saved 
+# this imports all trials within a .mat file and the accompanying header file
 allTrials = sp.loadmat('alltrials_testing_2021_0912.mat', squeeze_me = True)
 allTrialsData = allTrials['trials']
 header = allTrials['header']

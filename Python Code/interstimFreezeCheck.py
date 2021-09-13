3d3417a616f1ca71491338f3e164bdd005843a9a
cherianchery <cherianchery@uchicaog.edu>
2021-09-12 22:33:25 -0500

    now importing header along with struct of trials

--- Python Code/interstimFreezeCheck.py
+++ Python Code/interstimFreezeCheck.py
@@ -1,3 +1,14 @@
 import scipy.io as sp
 import numpy as np
 
+# when importing .mat file, make sure it is the combined file with both the 
+# header and all trials saved 
+
+allTrials = sp.loadmat('alltrials_testing_2021_0912.mat', squeeze_me = True)
+allTrialsData = allTrials['trials']
+header = allTrials['header']
+
+frameRateHz = header['frameRateHz'].item()['data'].tolist()
+stimDuration = header['stimSetting'].item()['data'].item()['stimDurationMS'].tolist()
+
+

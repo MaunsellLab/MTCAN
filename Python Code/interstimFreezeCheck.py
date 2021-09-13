5f2df94abcf4a8321e1d8efe2f34fffa3290ead0
cherianchery <cherianchery@uchicaog.edu>
2021-09-12 22:43:55 -0500

    Update interstimFreezeCheck.py

--- Python Code/interstimFreezeCheck.py
+++ Python Code/interstimFreezeCheck.py
@@ -3,13 +3,12 @@ import numpy as np
 
 # when importing .mat file, make sure it is the combined file with both the 
 # header and all trials saved 
-
 allTrials = sp.loadmat('alltrials_testing_2021_0912.mat', squeeze_me = True)
 allTrialsData = allTrials['trials']
 header = allTrials['header']
 
 frameRateHz = header['frameRateHz'].item()['data'].tolist()
-stimDuration = header['stimSetting'].item()['data'].item()['stimDurationMS'].tolist()
+stimDur = header['stimSetting'].item()['data'].item()['stimDurationMS'].tolist()
 
 
 for i in range(0,len(allTrialsData.item()[0])-1):

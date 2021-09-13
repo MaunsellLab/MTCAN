ceda33e10ed8291962f8ccd8e0e8acb44e692c4f
cherianchery <cherianchery@uchicaog.edu>
2021-09-12 22:53:20 -0500

    Update interstimFreezeCheck.py

--- Python Code/interstimFreezeCheck.py
+++ Python Code/interstimFreezeCheck.py
@@ -1,12 +1,6 @@
 import scipy.io as sp
 import numpy as np
 
-# when importing .mat file, make sure it is the combined file with both the 
-# header and all trials saved 
-allTrials = sp.loadmat('alltrials_testing_2021_0912.mat', squeeze_me = True)
-allTrialsData = allTrials['trials']
-
-
 def interstimFreezeCheck(currTrial):
     '''
     takes the current trial and sees if the stimulus presentations have frozen

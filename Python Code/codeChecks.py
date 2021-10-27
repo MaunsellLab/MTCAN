018c9e66a7de18cdd4079cd18d7e176302affefc
cherianchery <cherianchery@uchicaog.edu>
2021-10-26 23:13:21 -0500

    Update codeChecks.py
    code to check for actual saccade times vs reported times, plotting eye-pos with respect to target onset

--- Python Code/codeChecks.py
+++ Python Code/codeChecks.py
@@ -1,3 +1,10 @@
+import scipy.io as sp
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+allTrials = sp.loadmat('Meetz_2021_1021_4.mat', squeeze_me = True)
+allTrialsData = allTrials['trials']
 
 # code to check whether valid RT are being rewarded or not
 # check EOT to make sure that trialEnd is not including 'wrong/distracted' 
@@ -23,23 +30,71 @@ for n,currTrial in enumerate(allTrialsData.item()[0]):
 earlyWithRT = []
 for n, currTrial in enumerate(allTrialsData.item()[0]):
     extendedEOT = currTrial['extendedEOT'].item()['data']
-        if extendedEOT == 6 and 'targetOn' in currTrial.dtype.names and 'reactTimeMS' in currTrial.dtype.names:
-            earlyWithRT.append(currTrial['reactTimeMS'].item()['data'].item())
+    if extendedEOT == 6 and 'targetOn' in currTrial.dtype.names and 'reactTimeMS' in currTrial.dtype.names:
+        earlyWithRT.append(currTrial['reactTimeMS'].item()['data'].item())
+
+# Short trials with short RT
+correctShortRT = []
+for n, currTrial in enumerate(allTrialsData.item()[0]):
+    extendedEOT = currTrial['extendedEOT'].item()['data']
+    trial = currTrial['trial'].item()['data'].item()
+    if extendedEOT == 0 and trial['catchTrial'] != 1:
+        reactTimeMS = currTrial['reactTimeMS'].item()['data'].item()
+        if reactTimeMS <100:
+            correctShortRT.append((n, reactTimeMS))
 
 # convert Left eyeXY to eyePos
-eyeLX = currTrial['eyeLXData'].item()['data'].item() 
-eyeLY = currTrial['eyeLYData'].item()['data'].item()
-eyeRX = currTrial['eyeRXData'].item()['data'].item()
-eyeRY = currTrial['eyeRYData'].item()['data'].item()
-eyeLeftCal = currTrial['eyeLeftCalibrationData'].item()['data'].itm()['cal'].item()
-eyeRightCal = currTrial['eyeRightCalibrationData'].item()['data'].item()['cal'].item()
+for i in correctShortRT:
+    trialNumber, reactTime = i
+    currTrial = allTrialsData.item()[0][trialNumber]
+    leftXDeg = []
+    leftYDeg = []
+    rightXDeg = []
+    rightYDeg = []
+    eyeLX = currTrial['eyeLXData'].item()['data'].item() 
+    eyeLY = currTrial['eyeLYData'].item()['data'].item()
+    eyeRX = currTrial['eyeRXData'].item()['data'].item()
+    eyeRY = currTrial['eyeRYData'].item()['data'].item()
+    eyeLeftCal = currTrial['eyeLeftCalibrationData'].item()['data'].item()['cal'].item()
+    eyeRightCal = currTrial['eyeRightCalibrationData'].item()['data'].item()['cal'].item()
 
-for n,eyePos in enumerate(eyeLX):
-    xDeg = (eyePos * eyeLeftCal['m11'].item()) + (eyeLY[n] * eyeLeftCal['m21'].item()) + eyeLeftCal['tX'].item()
-    yDeg = (eyePos * eyeLeftCal['m12'].item()) + (eyeLY[n]) * eyeLeftCal['m22'].item() + eyeLeftCal['tY'].item()
+    count = min([len(eyeLX), len(eyeLY), len(eyeRX), len(eyeRY)])
+    for l in range(0, count):
+        xDegConvert = (eyeLX[l] * eyeLeftCal['m11'].item()) + (eyeLY[l] * eyeLeftCal['m21'].item()) + eyeLeftCal['tX'].item()
+        leftXDeg.append(xDegConvert)
+        yDegConvert = (eyeLX[l] * eyeLeftCal['m12'].item()) + (eyeLY[l] * eyeLeftCal['m22'].item()) + eyeLeftCal['tY'].item()
+        leftYDeg.append(yDegConvert)
+    for r in range(0, count):
+        xDegConvert = (eyeRX[r] * eyeRightCal['m11'].item()) + (eyeRY[r] * eyeRightCal['m21'].item()) + eyeRightCal['tX'].item()
+        rightXDeg.append(xDegConvert)
+        yDegConvert = (eyeRX[r] * eyeRightCal['m12'].item()) + (eyeRY[r] * eyeRightCal['m22'].item()) + eyeRightCal['tY'].item()
+        rightYDeg.append(yDegConvert)
 
-for n, eyePos in enumerate(eyeRX):
-    xDeg = (eyePos * eyeRightCal['m11'].item()) + (eyeRY[n]) * eyeRightCal['m21'].item()) + eyeRightCal['tX'].item()
-    yDeg = (eyePos * eyeRightCal['m12'].item()) + (eyeRY[n]) * eyeRightCal['m22'].item()) + eyeRightCal['tY'].item()
+    fixate = currTrial['fixate'].item()['timeMS'].item()
+    trialStart = currTrial['trialStart'].item()['timeMS'].item()
+    fixateTimeWRTEyePos = math.floor((fixate - trialStart)/2)
+    fixateTimeForIndex = fixateTimeWRTEyePos
+    windowWidth = currTrial['fixWindowData'].item()['data'].item()['windowDeg'].item()['size'].item()['width'].item()
+    windowHeight = currTrial['fixWindowData'].item()['data'].item()['windowDeg'].item()['size'].item()['height'].item()
+    for index, xDeg in enumerate(leftXDeg[fixateTimeWRTEyePos:]):
+        if -(windowWidth/2) > xDeg or xDeg > (windowWidth/2) or -(windowHeight/2) > leftYDeg[fixateTimeForIndex] or leftYDeg[fixateTimeForIndex] > (windowHeight/2):
+            eyePosSac = index + fixateTimeWRTEyePos
+            break
+        else:
+            fixateTimeForIndex += 1
+    eyePosSacTimeMS = eyePosSac * 2
+    sacFromTrialStart = currTrial['saccade'].item()['timeMS'].item() - trialStart
+    diff = sacFromTrialStart - eyePosSacTimeMS
+    print(trialNumber, diff)
 
+    targetOn = currTrial['targetOn'].item()['timeMS'].item()
+    trialStart = currTrial['trialStart'].item()['timeMS'].item()
+    targetOnTimeEyePos = (targetOn - trialStart)/2
 
+    plt.figure()
+    plt.plot(leftXDeg, color = 'olive')
+    plt.plot(leftYDeg, color = 'green')
+    plt.plot(rightXDeg, color = 'blue')
+    plt.plot(rightYDeg, color = 'red')
+    plt.axvline(x = targetOnTimeEyePos)
+    plt.title(reactTime)

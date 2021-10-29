b8558d59ebb481422fd57f87575d8ec4048cc72d
cherianchery <cherianchery@uchicaog.edu>
2021-10-29 11:55:33 -0500

    Update usefulFns.py
    added some useful functions

--- Python Code/usefulFns.py
+++ Python Code/usefulFns.py
@@ -1,3 +1,15 @@
+def loadMatFile(fileName):
+    '''
+    Loads the given matfile and assigns variables to access trial data
+
+    Inputs: matfile name, str
+    Outputs: variables, nd. array
+    '''
+    allTrials = sp.loadmat(fileName, squeeze_me = True)
+    allTrialsData = allTrials['trials']
+    header = allTrials['header']
+
+
 def fieldInTrial(trial, fieldList):
     '''
     Function will check whether a field or all fields in the list is in the trial
@@ -13,4 +25,18 @@ def fieldInTrial(trial, fieldList):
         else:
             return True
 
-            
+def targetOnsetIndexStimDesc(stimDesc):
+    '''
+    fn will identify the index of the target onset stimulus
+    fn will put out ListTypes subdictionary within stimDesc 
+    
+    Inputs: stimDesc (variable)
+    Outputs: the index of the target onset stim (int)
+    '''
+
+    for count, d in enumerate(stimDesc['listTypes']):
+                if 2 in d:
+                    targetOnsetStim = count
+                    break
+    
+    return count

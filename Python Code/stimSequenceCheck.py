a0240ca3be13531432e75e4614c74cb0ba9d427d
cherianchery <cherianchery@uchicaog.edu>
2021-11-02 17:34:35 -0500

    Update stimSequenceCheck.py
    argparse added, so that I can run script through command-line

--- Python Code/stimSequenceCheck.py
+++ Python Code/stimSequenceCheck.py
@@ -1,20 +1,18 @@
-import scipy.io as sp
-import numpy as np
-import matplotlib.pyplot as plt
+'''
+This script is intended to make sure stimulus sequences and interstim times 
+are frozen. 
+The .mat files should have both the header and the trials fields to ensure
+that the script runs smoothly. 
+'''
+
 from usefulFns import *
+import argparse
 
-# when importing .mat file, make sure it is the combined file with both the 
-# header and all trials saved 
-# this imports all trials within a .mat file and the accompanying header file
+parser = argparse.ArgumentParser('')
+parser.add_argument('fileName', type=str)
+args = parser.parse_args()
 
-allTrialsData, header = loadMatFile('Meetz_2021_08_25.mat')
-
-# allTrials = sp.loadmat('Meetz_2021_08_25.mat', squeeze_me = True)
-# allTrialsData = allTrials['trials']
-# header = allTrials['header']
-
-global locDict
-
+allTrialsData, header = loadMatFile(args.fileName)
 locDict = {0:{'seq':[],'count':[1]*9,'interstim':[]},
            1:{'seq':[],'count':[1]*9,'interstim':[]},
            2:{'seq':[],'count':[1]*9,'interstim':[]},
@@ -23,7 +21,7 @@ lastStim = [[],[],[],[]]
 currStim = [[],[],[],[]]
 
 
-def stimSeqCheck(attendLoc):
+def stimSeqCheck(attendLoc, stimSeqLen):
     '''
     Function to create a stimulus seq based on attended location and this 
     function will also ensure that subsequence trials follow the stimulus
@@ -31,8 +29,8 @@ def stimSeqCheck(attendLoc):
     of interstim frames in between each stimulus config and check to see that
     the list is frozen during entire dataset.
 
-    inputs: attendedLoc (integer)
-    outputs: appends to locDict (dictionary of dictionaries), where 0,1,2,3 
+    Inputs: attendedLoc (integer)
+    Outputs: appends to locDict (dictionary of dictionaries), where 0,1,2,3 
              describe the attended location
              print statement if something is out of sequence (stim sequence or
              interstim sequence)
@@ -40,16 +38,16 @@ def stimSeqCheck(attendLoc):
 
 
     frameOff = stimDesc['stimOffFrame'][0]
-    for d in stimDesc[1:targetOnsetStim]:
+    for d in stimDesc[1:stimSeqLen]:
         if len(locDict[attendLoc]['seq']) < 9:
-            locDict[attendLoc]['seq'].append(d['stimTypes'][2:4].tolist())
-            lastStim[attendLoc] = d['stimTypes'][2:4].tolist()
+            locDict[attendLoc]['seq'].append(d['stimTypes'][:2].tolist())
+            lastStim[attendLoc] = d['stimTypes'][:2].tolist()
             interstim = d['stimOnFrame'] - frameOff
             frameOff = d['stimOffFrame']
             locDict[attendLoc]['interstim'].append(interstim)
         else:
             indexLastStim = locDict[attendLoc]['seq'].index(lastStim[attendLoc])
-            currStim[attendLoc] = d['stimTypes'][2:4].tolist()
+            currStim[attendLoc] = d['stimTypes'][:2].tolist()
             indexCurrStim = locDict[attendLoc]['seq'].index(currStim[attendLoc])
             locDict[attendLoc]['count'][indexCurrStim] += 1
             if indexLastStim == len(locDict[attendLoc]['seq']) - 1:
@@ -71,13 +69,18 @@ for currTrial in allTrialsData.item()[0]:
 
     if trialEnd == 0: 
         if trial['instructTrial'] == 0:
-            stimDesc = currTrial['stimDesc'].item()['data'].item()
-            for count, d in enumerate(stimDesc['listTypes']):
-                if 2 in d:
-                    targetOnsetStim = count
-                    break
-            attendLoc = trial['attendLoc'].tolist()
-
-            stimSeqCheck(attendLoc)
+            if trial['catchTrial'] != 1:
+                stimDesc = currTrial['stimDesc'].item()['data'].item()
+                for count, d in enumerate(stimDesc['listTypes']):
+                    if 2 in d:
+                        stimSeqLen = count
+                        break
+                attendLoc = trial['attendLoc'].tolist()
+                stimSeqCheck(attendLoc, stimSeqLen)
+            else:
+                stimDesc = currTrial['stimDesc'].item()['data'].item()
+                stimSeqLen = len(stimDesc['listTypes']) + 1 
+                attendLoc = trial['attendLoc'].tolist()
+                stimSeqCheck(attendLoc, stimSeqLen)   
 
 print(locDict)

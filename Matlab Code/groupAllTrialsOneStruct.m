a5591b8ac0c0d0635acae481d61c3f96d23462f6
cherianchery <cherianchery@uchicago.edu>
2022-02-11 22:53:52 -0600

    Update groupAllTrialsOneStruct.m

--- Matlab Code/groupAllTrialsOneStruct.m
+++ Matlab Code/groupAllTrialsOneStruct.m
@@ -1,13 +1,19 @@
 clear 
 
-fileName = 'Meetz_2022_0114_MTNAN3.dat';
+fileName = 'Meetz_2022_0211_MTC.dat';
 
 header = readLLFile('i', fileName);
-trials = struct();
+trials = {};
 for i = 1:header.numberOfTrials
-    trials.trials{i} = readLLFile('t', i);
+    trials{i} = readLLFile('t', i);
 end
+
+% for t = 1:length(trials)
+%     trials{1,t}.spikeData = -ones([5000, 3]);
+% end
 
 cd '../Matlab Data'/
 fileName = strrep(fileName, '.dat', '.mat');
-save(fileName, 'trials', 'header');
+save(fileName, 'trials', 'header')
+% use code below to save as matlab 7.3 version
+% save(fileName, 'trials', 'header', '-v7.3');

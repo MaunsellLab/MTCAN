bcf294e2f2e9571e68a04f2bc520473164c922be
cherianchery <cherianchery@uchicago.edu>
2022-02-14 17:50:31 -0600

    Update insertSpikeDataIntoTrials.m

--- Matlab Code/insertSpikeDataIntoTrials.m
+++ Matlab Code/insertSpikeDataIntoTrials.m
@@ -1,25 +1,33 @@
 % Extracts channel, unit, timestamp from Nev file and inserts into apt
 % trial in the .mat (originally .dat) behavioral file. 
 
-trialsCount = 1
+eventDict = taskDict();
+trialsCount = 1;
 
 for i = 1:length(NEV)
     if NEV(i,1) == 0 && NEV(i,2) == 54355  %trialStart event code 
         trialStartIndex = i;
         chan0Counter = 0;
-        for j = trialStartIndex+1:(length(NEV)-1)
-            while chan0Counter < 1;
-                if NEV(j,1) == 0
+        for j = trialStartIndex+1:length(NEV)
+            while chan0Counter < 1
+                if NEV(j,1) == 0 && ~ismember(NEV(j,2), eventDict.codes)
                     nevTrialCounter = NEV(j,2);
                     chan0Counter = chan0Counter + 1;
                 end
             end
             if NEV(j,1) == 0 && NEV(j,2) == 54341 %trialEnd event code
-                trialEndIndex = j;
-                for t = trialsCount:length(trials)
-                    if trials{1,t}.trialStart.data == nevTrialCounter
-                        trials{1,t}.spikeData = NEV(i:trialEndIndex,:);
-                        trialsCount = trialsCount + 1;
+                for x = j+1:length(NEV)
+                    while chan0Counter < 2
+                        if NEV(x,1) == 0
+                            trialEndIndex = x;
+                            for t = 1:length(trials)
+                                if trials{1,t}.trialStart.data == nevTrialCounter
+                                    trials{1,t}.spikeData = NEV(trialStartIndex:trialEndIndex,:);
+                                    trialsCount = trialsCount + 1;
+                                end
+                            end
+                            chan0Counter = chan0Counter + 1;
+                        end 
                     end
                 end
                 break

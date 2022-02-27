632ce974805ea7ac250d9f886fdaba5c53d63ee3
cherianchery <cherianchery@uchicago.edu>
2022-02-27 11:51:32 -0600

    Update groupAllTrialsOneStruct.m

--- Matlab Code/groupAllTrialsOneStruct.m
+++ Matlab Code/groupAllTrialsOneStruct.m
@@ -1,19 +1,90 @@
-clear 
+clear
 
-fileName = 'testing_220214_MTC.dat';
+baseFileName = 'testing_220225';
 
-header = readLLFile('i', fileName);
-trials = {};
-for i = 1:header.numberOfTrials
-    trials{i} = readLLFile('t', i);
+directory = '/Users/chery/Documents/Grad School/Maunsell Lab/Analysis/MTCAN/Lablib Data/';
+
+fileNameMTC = [baseFileName '_MTC.dat'];
+fileNameMTN = [baseFileName '_MTN.dat'];
+fileNameGRF = [baseFileName '_GRF.dat'];
+
+if isfile(fileNameGRF)
+%     to convert GRF to same format as MTC/MTN
+%     header = readLLFile('i', fileNameGRF);
+%     trials = cell(1,header.numberOfTrials);
+%     for i = 1:header.numberOfTrials
+%         trials{i} = readLLFile('t', i);
+%     end
+%     
+%     cd '../Matlab Data'/
+%     fileNameGRF = strrep(fileNameGRF, '.dat', '.mat');
+%     save(fileNameGRF, 'trials', 'header')
+    convertGRF(fileNameGRF);
+    cd '..'/'Lablib Data'/
 end
 
-% for t = 1:length(trials)
-%     trials{1,t}.spikeData = -ones([5000, 3]);
-% end
+if isfile(fileNameMTC)
+    header = readLLFile('i', fileNameMTC);
+    trials = cell(1,header.numberOfTrials);
+    for i = 1:header.numberOfTrials
+        trials{i} = readLLFile('t', i);
+    end
+    
+    cd '../Matlab Data'/
+    fileNameMTC = strrep(fileNameMTC, '.dat', '.mat');
+    save(fileNameMTC, 'trials', 'header')
+    cd '..'/'Lablib Data'/
+end
 
-cd '../Matlab Data'/
-fileName = strrep(fileName, '.dat', '.mat');
-save(fileName, 'trials', 'header')
+if isfile(fileNameMTN)
+    header = readLLFile('i', fileNameMTN);
+    trials = cell(1,header.numberOfTrials);
+    for i = 1:header.numberOfTrials
+        trials{i} = readLLFile('t', i);
+    end
+    
+    cd '../Matlab Data'/
+    fileNameMTN = strrep(fileNameMTN, '.dat', '.mat');
+    save(fileNameMTN, 'trials', 'header')
+    cd '..'/'Lablib Data'/
+end
+
+
+
+
+% fileName = 'testing_220214_MTC.dat';
+% 
+% header = readLLFile('i', fileName);
+% 
+% trials = {};
+% for i = 1:header.numberOfTrials
+%     trials{i} = readLLFile('t', i);
+% end
+% 
+% cd '../Matlab Data'/
+% fileName = strrep(fileName, '.dat', '.mat');
+% save(fileName, 'trials', 'header')
+
 % use code below to save as matlab 7.3 version
 % save(fileName, 'trials', 'header', '-v7.3');
+
+
+% if contains(fileName, 'MTC')
+%     trialsMTC = trials;
+%     cd '../Matlab Data'/
+%     fileName = strrep(fileName, '.dat', '.mat');
+%     save(fileName, 'trialsMTC', 'header')
+% elseif contains(fileName, 'MTN')
+%     trialsMTN = trials;
+%     cd '../Matlab Data'/
+%     fileName = strrep(fileName, '.dat', '.mat');
+%     save(fileName, 'trialsMTN', 'header')
+% elseif contains(fileName, 'GRF')
+%     trialsGRF = trials;
+%     cd '../Matlab Data'/
+%     fileName = strrep(fileName, '.dat', '.mat');
+%     save(fileName, 'trialsGRF', 'header')
+% end
+% 
+% clear trials
+

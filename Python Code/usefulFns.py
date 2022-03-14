c38b2c015834b96a2ce2df782d40641a18e4037f
cherianchery <cherianchery@uchicago.edu>
2022-03-13 20:14:20 -0500

    Update usefulFns.py

--- Python Code/usefulFns.py
+++ Python Code/usefulFns.py
@@ -124,24 +124,116 @@ def eyePosDurTrial(currTrial):
     return eyesXYDeg
 
 
-
-def activeUnits(spikeData):
+def activeUnits(unitData):
 
     '''
     function returns the active units across trials for a session as a list
 
-    Inputs: unitData (str): spikeData
+    Inputs: unitData (str): Are we using fakeData or spikeData
     Outputs: units (list): active units for a sessioon
 
     '''
     units = []
     for currTrial in allTrials:
-        if spikeData in currTrial:
-            uniqueUnits = np.unique(currTrial[spikeData]['unit'])
+        if unitData in currTrial:
+            uniqueUnits = np.unique(currTrial[unitData]['unit'])
             for unique in uniqueUnits:
                 if unique not in units:
                     units.append(unique)
     
     return units
+
+
+def insertStimSpikeData(units, index, stimOnTimeSNEV):
+    '''
+    this function will generate a poisson spike train and return the normalized
+    response for the RF for a given stimulus configuration
+    Inputs:
+        x: neuron number
+        index: the index of the corresponding stimulus configuration
+        sigma: semisaturation constant from neuron's contrast response function
+    Outputs:
+        RFSpikes: normalized response 
+
+    spikes_4rows = np.tile(spikes, (4,1))
+    '''
+
+
+    numNeurons = len(units)
+
+    C0 = stimIndexDict[index][0]['contrast']
+    C1 = stimIndexDict[index][1]['contrast']
+    L0 = tcDict[1][stimIndexDict[index][0]['direction']]
+    L1 = tcDict[1][stimIndexDict[index][1]['direction']]
+    sigma = 0.1
+    expectedNormSpikeRate = int(((C0*L0) + (C1*L1))/(C0 + C1 + sigma))
+    stimDur = 493
+    popMean = expectedNormSpikeRate/(1000/stimDur)
+
+    spikes = popMean + np.random.rand(1,numNeurons)           
+    R = np.zeros((numNeurons,numNeurons))
+    for neuronI in range(numNeurons):
+        for neuronJ in range(numNeurons):
+            if neuronI == neuronJ:
+                R[neuronI,neuronJ] = 1
+            else:
+                R[neuronI, neuronJ] = 0.1
+
+    L = np.linalg.cholesky(R)
+    spikes = np.matmul(spikes,L)   
+    spikes = np.around(spikes) 
+
+    # print(spikes)
+
+
+    for count, i in enumerate(spikes[0]):
+        if i != 0:
+            unit = units[count]
+            channelIdentity = int(unit[0:unit.find('_')])
+            channel = np.array([channelIdentity] * stimDur)
+            spikeTimeMS = (np.sort(np.random.choice(np.arange(stimDur), int(i),
+            replace = False)))/1000
+            currTrial['spikeData']['timeStamp'] = np.append(currTrial['spikeData'] \
+            ['timeStamp'], stimOnTimeSNEV + spikeTimeMS, 0)
+            currTrial['spikeData']['unit'] = np.append(currTrial['spikeData'] \
+            ['unit'], [unit] * len(spikeTimeMS), 0)
+            currTrial['spikeData']['channel'] = np.append(currTrial['spikeData'] \
+            ['channel'], [channelIdentity] * len(spikeTimeMS), 0)
+
+
+def randTuningCurve(numNeurons):
+    '''
+    Functon will generate random tuning cruves for x number of neurons.
+    Function will also iterate through tuning curves to create dictionary of 
+    responses to each direction for each neuron 
+
+    Inputs:
+        numNueurons (int): number of neurons 
+    Outputs:
+        tuningMat (2D array): matrix of tuning curve values for each
+                              neuron
+        tcDictionary (dictionary): maps each neurons response for a direction onto
+                                   a dictionary
+    '''
+    tuningMat = np.zeros((numNeurons + 1, 6))
+    tuningMat[0] = np.arange(0,360,60)
+    tcDictionary = {}
+
+    for i in range(1, tuningMat.shape[0]):
+        np.random.seed(2)
+        amp = np.random.randint(15,30)
+        y_translate = np.random.randint(30,50)
+        x_translate = np.random.randint(60,120)
+        tuningMat[i,:] = (amp * np.sin((tuningMat[0,:] * (np.pi / 180)) + x_translate)) + y_translate
+
+    for i, neuron in enumerate(tuningMat[1:,:]):
+        tcDictionary[i+1] = {}
+        for j, dirResp in enumerate(neuron):
+            tcDictionary[i+1][tuningMat[0][j]] = dirResp
+
+    return tuningMat, tcDictionary
+
+
+
 #testing time to open file
 # t0 = time.time(); a = mat73.loadmat('./Documents/Grad School/Maunsell Lab/Analysis/MTCAN/Matlab Data/Meetz_2022_0114_MTNAN_m73.mat', use_attrdict = True); print(time.time() - t0)

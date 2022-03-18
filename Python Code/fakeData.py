6da858d89fa945069eb1918628828a8c84289354
cherianchery <cherianchery@uchicago.edu>
2022-03-18 13:47:24 -0500

    Update fakeData.py

--- Python Code/fakeData.py
+++ Python Code/fakeData.py
@@ -293,6 +293,58 @@ for currTrial in allTrials:
                     spikeData['channel'] = np.append(spikeData['channel'], \
                                             [channelIdentity] * len(spikeTimeS), 0)
 
+
+# fake spikes for speed tuning
+speedMat = [40,80,40,20] #spikes/sec at each speed index
+baseFR = 10 #spikes/sec
+dt = 1/1000
+
+for currTrial in allTrials:
+    trial = currTrial['trial']['data']
+    trialEnd = currTrial['trialEnd']['data']
+    if trial['instructTrial'] != 1 and trialEnd == 0:
+        trialStartMS = currTrial['trialStart']['timeMS']
+        trialStartSNEV = currTrial['taskEvents']['trialStart']['timeS']
+        trialEndSNEV = currTrial['taskEvents']['trialEnd']['timeS']
+        trialLen = trialEndSNEV - trialStartSNEV
+        stimDesc = currTrial['stimDesc']
+        spikeData = currTrial['spikeData']
+        spikeData['channel'] = []
+        spikeData['unit'] = []
+        spikeData['timeStamp'] = []
+        for unit in units:
+            channelIdentity = int(unit[0:unit.find('_')])
+            spikes = np.random.poisson(trialLen*baseFR)
+            spikeTimeS = (np.sort(np.random.choice(np.arange(trialLen * 1000),\
+                           spikes, replace = False)))/1000
+            spikeData['timeStamp'] = np.append(spikeData['timeStamp'], \
+                                     trialStartSNEV + spikeTimeS, 0)
+            spikeData['unit'] = np.append(spikeData['unit'], \
+                                [unit] * len(spikeTimeS), 0)
+            spikeData['channel'] = np.append(spikeData['channel'], \
+                                   [channelIdentity] * len(spikeTimeS), 0)
+        for count, stim in enumerate(stimDesc['data']):
+            if stim['stimType'] == 2:
+                break
+            if stim['gaborIndex'] == 1:
+                speedIndex = int(stim['temporalFreqIndex'])
+                stimOnTimeMS = stimDesc['timeMS'][count]
+                stimDiffS = (stimOnTimeMS - trialStartMS)/1000
+                stimOnSNEV = trialStartSNEV + stimDiffS
+                for unitCount, unit in enumerate(units):
+                    expectedSpikes = np.random.poisson(speedMat[speedIndex]*stimDurMS/1000)
+                    channelIdentity = int(unit[0:unit.find('_')])
+                    spikeTimeS = (np.sort(np.random.choice(np.arange(stimDurMS),\
+                    expectedSpikes, replace=False)))/1000
+                    spikeData['timeStamp'] = np.append(spikeData['timeStamp'],\
+                                            stimOnSNEV + spikeTimeS, 0)
+                    spikeData['unit'] = np.append(spikeData['unit'], \
+                                        [unit] * len(spikeTimeS), 0)
+                    spikeData['channel'] = np.append(spikeData['channel'], \
+                                            [channelIdentity] * len(spikeTimeS), 0)
+
+
+
 '''
 old verison pre v7.3
 '''
@@ -328,8 +380,7 @@ for currTrial in allTrials:
         currTrial['fakeData']['unit'] = np.array(currTrial['fakeData']['unit'])              
 
 '''
-57.085
-58.212
+
 def poissonSpikeTrain(x, index):
     '''
     this function will generate a poisson spike train and return the normalized
@@ -422,118 +473,6 @@ def insertStimSpikeData(x, index, stimOnTimeSNEV):
         currTrial['spikeData']['channel'] = np.append(currTrial['spikeData'] \
         ['channel'], [channelIdentity] * len(spikeIndexS), 0)
 
-# for generating underlying base firing rate
-for currTrial in allTrials:
-    if 'spikeData' in currTrial:
-        trialStartS = currTrial['taskEvents']['trialStart']['timeS']
-        trialEndS = currTrial['taskEvents']['trialEnd']['timeS']
-        trialLen = int(round(trialEndS - trialStartS, 3) * 1000)
-        # generate underlying firing rate 
-        for count, unit in enumerate(units):
-            channelIdentity = int(unit[0:unit.find('_')])
-            baseFR = 5 # same firing rate for each neuron
-            dt = 1/1000
-            baseSpikeTrain = [0] * trialLen
-            for i in range(trialLen):
-                if np.random.uniform(0,1) < baseFR * dt:
-                    baseSpikeTrain[i] = 1
-            spikeIndex = np.where(np.array(baseSpikeTrain) == 1)
-            spikeIndexS = spikeIndex[0]/1000 #[0] for tuple 
-            if count == 0:
-                currTrial['spikeData']['timeStamp'] = trialStartS + spikeIndexS
-                currTrial['spikeData']['unit'] = [unit] * len(spikeIndexS)
-                currTrial['spikeData']['channel'] = [channelIdentity] * len(spikeIndexS)
-            else:
-                currTrial['spikeData']['timeStamp'] = np.append(currTrial \
-                ['spikeData']['timeStamp'], trialStartS + spikeIndexS, 0)
-                currTrial['spikeData']['unit'] = np.append(currTrial['spikeData'] \
-                ['unit'], [unit] * len(spikeIndexS), 0)
-                currTrial['spikeData']['channel'] = np.append(currTrial['spikeData'] \
-                ['channel'], [channelIdentity] * len(spikeIndexS), 0)
-                # extend the currTrial['spikeData']['timeStamp'] fields
-        stimDesc = currTrial['stimDesc']['data']
-        for count, stim in enumerate(stimDesc):
-            if stim['stimLoc'] == 0 and stim['listType'] == 1:
-                index = int(stim['stimIndex'].tolist())
-                stimOnTimeMS = currTrial['stimDesc']['timeMS'][count] - \
-                currTrial['trialStart']['timeMS']
-                stimOnTimeSNEV = round(currTrial['taskEvents']['trialStart']\
-                ['timeS'].tolist() + (stimOnTimeMS/1000), 3)
-                for j in range(0, len(units)):
-                    insertStimSpikeData(j, index, stimOnTimeSNEV)
-
-# code will add stimuli presentations to index matrix, excluding padding
-# stimuli and when target appears before RF stimulus turns off
-for n,currTrial in enumerate(allTrialsData.item()[0]):
-    extendedEOT = currTrial['extendedEOT'].item()['data'].item()
-    trial = currTrial['trial'].item()['data'].item()
-    targetOnFrame = 20000
-    if extendedEOT == 0 and trial['instructTrial'] != 1:
-        stimDesc = currTrial['stimDesc'].item()['data'].item()
-        for n,stim in enumerate(stimDesc):
-            if stim['listType'] == 2:
-                targetIndex = n
-                break
-        targetOnFrame = stimDesc['stimOnFrame'][targetIndex]
-        stimCount = 0
-        for stim in stimDesc:
-            if stim['stimLoc'] == 0 and stim0Count == 0:
-                stim0Count += 1
-            elif stim['stimLoc'] == 0 and stim0Count != 0 and stim['stimOffFrame'] < targetOnFrame:
-                index = stim['stimIndex']
-                stimIndexCount[index] = stimIndexCount.get(index, 0) + 1
-
-                for x in range(1,numNeurons+1):
-                    spikeCountMat[x-1][stimIndexCount[index]][index] = poissonSpikeTrain(x, index)
-
-
-meanSpike = np.nanmean(spikeCountMat[:,1:,:], axis = 1)
-
-meanSpikeReshaped = np.zeros((len(units),1,169))
-for count,i in enumerate(meanSpikeReshaped):
-    i[:,0:6] = meanSpike[count][0:6]
-    i[:,6:12] = meanSpike[count][36:42]
-    i[:,12] = meanSpike[count][156]
-    i[:,13:19] = meanSpike[count][6:12]
-    i[:,19:25] = meanSpike[count][42:48]
-    i[:,25] = meanSpike[count][157]
-    i[:,26:32] = meanSpike[count][12:18]
-    i[:,32:38] = meanSpike[count][48:54]
-    i[:,38] = meanSpike[count][158]
-    i[:,39:45] = meanSpike[count][18:24]
-    i[:,45:51] = meanSpike[count][54:60]
-    i[:,51] = meanSpike[count][159]
-    i[:,52:58] = meanSpike[count][24:30]
-    i[:,58:64] = meanSpike[count][60:66]
-    i[:,64] = meanSpike[count][160]
-    i[:,65:71] = meanSpike[count][30:36]
-    i[:,71:77] = meanSpike[count][66:72]
-    i[:,77] = meanSpike[count][161]
-    i[:,78:84] = meanSpike[count][72:78]
-    i[:,84:90] = meanSpike[count][108:114]
-    i[:,90] = meanSpike[count][162]
-    i[:,91:97] = meanSpike[count][78:84]
-    i[:,97:103] = meanSpike[count][114:120]
-    i[:,103] = meanSpike[count][163]
-    i[:,104:110] = meanSpike[count][84:90]
-    i[:,110:116] = meanSpike[count][120:126]
-    i[:,116] = meanSpike[count][164]
-    i[:,117:123] = meanSpike[count][90:96]
-    i[:,123:129] = meanSpike[count][126:132]
-    i[:,129] = meanSpike[count][165]
-    i[:,130:136] = meanSpike[count][96:102]
-    i[:,136:142] = meanSpike[count][132:138]
-    i[:,142] = meanSpike[count][166]
-    i[:,143:149] = meanSpike[count][102:108]
-    i[:,149:155] = meanSpike[count][138:144]
-    i[:,155] = meanSpike[count][167]
-    i[:,156:168] = meanSpike[count][144:156]
-    i[:,168] = meanSpike[count][168]
-
-a = meanSpikeReshaped[0]
-b = a.reshape(13,13)
-plt.imshow(b, cmap='hot', interpolation='nearest')
-    
 
 # will print trial nunmbers that have targets that appear during RF stimulus presentation
 for count, currTrial in enumerate(allTrialsData.item()[0]):

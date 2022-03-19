3a810d210804ffa91819827447ea500f6ec88ec8
cherianchery <cherianchery@uchicago.edu>
2022-03-18 19:14:19 -0500

    Update RFTuning.py

--- Python Code/RFTuning.py
+++ Python Code/RFTuning.py
@@ -12,19 +12,6 @@ import matplotlib.pyplot as plt
 import matplotlib.patheffects as path_effects
 import seaborn as sns
 
-allTrials, header = loadMatFile73('testing_220310_Heatmap_GRF_Spikes.mat')
-
-# for testing purposes, to make unit field similar to real data
-for currTrial in allTrials:
-    if 'spikeData' in currTrial:
-        currTrial['spikeData']['unit'] = currTrial['spikeData']['unit'].tolist()
-        for i in range(0,len(currTrial['spikeData']['channel'])):
-            a = str(int(currTrial['spikeData']['channel'][i]))
-            b = str(int(currTrial['spikeData']['unit'][i]))
-            c = a + '_' + b
-            currTrial['spikeData']['unit'][i] = c
-        currTrial['spikeData']['unit'] = np.array(currTrial['spikeData']['unit'])
-
 def activeUnits(spikeData):
 
     '''
@@ -44,65 +31,72 @@ def activeUnits(spikeData):
     
     return units
 
+allTrials, header = loadMatFile73('testing_220310_Heatmap_GRF_Spikes.mat')
+
+# for testing purposes, to make unit field similar to real data
+for currTrial in allTrials:
+    if 'spikeData' in currTrial:
+        currTrial['spikeData']['unit'] = currTrial['spikeData']['unit'].tolist()
+        for i in range(0,len(currTrial['spikeData']['channel'])):
+            a = str(int(currTrial['spikeData']['channel'][i]))
+            b = str(int(currTrial['spikeData']['unit'][i]))
+            c = a + '_' + b
+            currTrial['spikeData']['unit'][i] = c
+        currTrial['spikeData']['unit'] = np.array(currTrial['spikeData']['unit'])
+
 units = activeUnits('spikeData')
+correctTrials = correctTrialsGRF(allTrials)
+
+numAzi = int(header['map0Settings']['data']['azimuthDeg']['n'].tolist())
+numEle = int(header['map0Settings']['data']['elevationDeg']['n'].tolist())
+stimDurMS = int(header['mapStimDurationMS']['data'].tolist())
+histPrePostMS = 50
 
 for unit in units:
-
-    numAzi = int(header['map0Settings']['data']['azimuthDeg']['n'].tolist())
-    numEle = int(header['map0Settings']['data']['elevationDeg']['n'].tolist())
-    stimDurMS = int(header['mapStimDurationMS']['data'].tolist())
-    histPrePostMS = 50
     
-
     stimCount = np.zeros((numEle,numAzi))
     spikeCountMat = np.zeros((50,numEle, numAzi))
     spikeCountMat[:,:,:] = np.nan
     spikeHists = np.zeros((stimDurMS + 2*histPrePostMS, numEle, numAzi))
 
-
-    for currTrial in allTrials:
-        trial = currTrial['trial']['data']
-        trialEnd = currTrial['trialEnd']['data']
-        if trial['instructTrial'] != 1 and trialEnd == 0:
-            trialStartMS = currTrial['trialStart']['timeMS']
-            trialStartSNEV = np.around(currTrial['taskEvents']['trialStart']['timeS'], 3)
-            stimDesc = currTrial['stimDesc']
-            spikeData = currTrial['spikeData']
-            for sCount, stim in enumerate(stimDesc['data']):
-                if stim['stimType'] == 2:
-                    break
-                if stim['gaborIndex'] == 1:
-                    aziIndex = int(stim['azimuthIndex'])
-                    eleIndex = int(stim['elevationIndex'])
-                    stCount = int(stimCount[eleIndex][aziIndex])
-                    stimOnTimeMS = stimDesc['timeMS'][sCount]
-                    stimDiffS = (stimOnTimeMS - trialStartMS)/1000
-                    stimOnSNEV = trialStartSNEV + stimDiffS
-                    if unit in spikeData['unit']:
-                        spikeData['timeStamp'] = np.around(spikeData['timeStamp'], 3)
-                        unitIndex = np.where(spikeData['unit'] == unit)
-                        unitTimeStamps = spikeData['timeStamp'][unitIndex]
-                        stimSpikes = np.where((unitTimeStamps >= stimOnSNEV) & 
-                                        (unitTimeStamps <= stimOnSNEV + stimDurMS/1000))
-                        spikeCountMat[stCount][eleIndex][aziIndex] = len(stimSpikes[0])
-                        stimCount[eleIndex][aziIndex] = stimCount[eleIndex][aziIndex] + 1
-                        
-                        #histograms
-                        # histSpikes = np.arange(stimOnSNEV - 0.050, stimOnSNEV + \
-                        #                     (stimDurMS+49)/1000, 0.001)
-                        # for histCount, i in enumerate(range(len(histSpikes))):
-                        #     if np.around(histSpikes[i],3) in unitTimeStamps:
-                        #         spikeHists[histCount][eleIndex][aziIndex] += 1
-                        
-                        stimOnPreSNEV = stimOnSNEV - 0.050
-                        stimOnPostSNEV = stimOnSNEV + (stimDurMS+49)/1000
-                        histStimSpikes = np.round((unitTimeStamps[((unitTimeStamps >= stimOnPreSNEV)\
-                                         & (unitTimeStamps <= stimOnPostSNEV))] - stimOnPreSNEV),3)
-                        for i in histStimSpikes:
-                            spikeHists[int(i*1000)][eleIndex][azIndex] += 1
-
-
-
+    for corrTrial in correctTrials:
+        currTrial = allTrials[corrTrial]
+        trialStartMS = currTrial['trialStart']['timeMS']
+        trialStartSNEV = np.around(currTrial['taskEvents']['trialStart']['timeS'], 3)
+        stimDesc = currTrial['stimDesc']
+        spikeData = currTrial['spikeData']
+        for sCount, stim in enumerate(stimDesc['data']):
+            if stim['stimType'] == 2:
+                break
+            if stim['gaborIndex'] == 1:
+                aziIndex = int(stim['azimuthIndex'])
+                eleIndex = int(stim['elevationIndex'])
+                stCount = int(stimCount[eleIndex][aziIndex])
+                stimOnTimeMS = stimDesc['timeMS'][sCount]
+                stimDiffS = (stimOnTimeMS - trialStartMS)/1000
+                stimOnSNEV = trialStartSNEV + stimDiffS
+                if unit in spikeData['unit']:
+                    spikeData['timeStamp'] = np.around(spikeData['timeStamp'], 3)
+                    unitIndex = np.where(spikeData['unit'] == unit)
+                    unitTimeStamps = spikeData['timeStamp'][unitIndex]
+                    stimSpikes = np.where((unitTimeStamps >= stimOnSNEV) & 
+                                    (unitTimeStamps <= stimOnSNEV + stimDurMS/1000))
+                    spikeCountMat[stCount][eleIndex][aziIndex] = len(stimSpikes[0])
+                    stimCount[eleIndex][aziIndex] = stimCount[eleIndex][aziIndex] + 1
+                    
+                    #histograms
+                    # histSpikes = np.arange(stimOnSNEV - 0.050, stimOnSNEV + \
+                    #                     (stimDurMS+49)/1000, 0.001)
+                    # for histCount, i in enumerate(range(len(histSpikes))):
+                    #     if np.around(histSpikes[i],3) in unitTimeStamps:
+                    #         spikeHists[histCount][eleIndex][aziIndex] += 1
+                    
+                    stimOnPreSNEV = stimOnSNEV - 0.050
+                    stimOnPostSNEV = stimOnSNEV + (stimDurMS+49)/1000
+                    histStimSpikes = unitTimeStamps[((unitTimeStamps >= stimOnPreSNEV)\
+                                        & (unitTimeStamps <= stimOnPostSNEV))] - stimOnPreSNEV
+                    histStimSpikes = np.int32(histStimSpikes*1000)
+                    spikeHists[histStimSpikes, eleIndex, aziIndex] += 1
 
 
     spikeCountMean = ma.mean(ma.masked_invalid(spikeCountMat), axis = 0)

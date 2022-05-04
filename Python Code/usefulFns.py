f38cf759fc11a85a04fcf914ea76cefe6a99e49e
cherianchery <cherianchery@uchicago.edu>
2022-05-04 10:09:23 -0500

    Update usefulFns.py

--- Python Code/usefulFns.py
+++ Python Code/usefulFns.py
@@ -1,10 +1,17 @@
 import scipy.io as sp
+from scipy.optimize import curve_fit
+from scipy import stats
+from itertools import combinations
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patheffects as path_effects
 import math
 import os
 from collections import defaultdict
 import mat73
+import seaborn as sns
+import matplotlib.ticker as ticker
+import time
 
 def loadMatFile73(NHP, date, fileName):
     '''
@@ -53,29 +60,62 @@ def correctTrialsGRF(allTrials):
     return correctTrials
 
 
-def correctTrialsNotInstruct(currTrial, catch=True):
+def correctTrialsMTX(allTrials):
     '''
-    Function will return a bool if the current trial is correct and not an 
-    instruction trial. Also has option to exclude catch trials 
+    Function will filter through allTrials and return a list of 
+    correct non-instruct trials in MTN/MTC. This function checks
+    for valid trialCertify trials (!=0)
 
-    Inputs: trial (nd.array)
-            catch (bool)
-    Outputs: bool
+    Inputs: allTrials (list of trials (nd.array))
+    Outputs: correctTrials(list): index of valid trials from allTrials
+    '''
+    correctTrials = []
+    for trialCount, currTrial in enumerate(allTrials):
+        trial = currTrial['trial']['data']
+        extendedEOT = currTrial['extendedEOT']['data']
+        trialCertify = currTrial['trialCertify']['data']
+        if trial['instructTrial'] != 1 and extendedEOT == 0 and trialCertify == 0:
+            correctTrials.append(trialCount)
+    
+    return correctTrials
+
+
+def gauss(x, H, A, x0, sigma):
+    '''
+    equation for gaussian fit
+    '''
+    return H + A * np.exp(-(x - x0) ** 2 / (2 * sigma ** 2))
+
+
+def gauss_fit(x, y):
+    '''
+    apply curve_fit from scipy.optimize 
+    '''
+    mean = sum(x * y) / sum(y)
+    sigma = np.sqrt(sum(y * (x - mean) ** 2) / sum(y))
+    popt, pcov = curve_fit(gauss, x, y, p0=[min(y), max(y), mean, sigma])
+    return popt
+
+
+def bhattCoef(m1, m2, v1, v2):
+    '''
+    This function will compute the Bhattacharyya Coefficient of two 
+    tuning curves. The BC measures how similar two normal distributions are. 
+
+    Inputs:
+        m1 (float) - mean of the first tuning curve
+        m2 (float) - mean of the second tuning curve
+        v1 (float) - variance of the first tuning curve
+        v2 (float) - variance of the second tuning curve
+    Outputs: 
+        BC (float) - a value b/w 0 and 1 that defines how similar the curves are.
     '''
 
-    extendedEOT = currTrial['extendedEOT'].item()['data'].item()
-    trial = currTrial['trial'].item()['data'].item()
-
-    if catch == True:
-        if extendedEOT == 0 and trial['instructTrial'] == 0:
-            return True
-        else:
-            return False
-    else:
-        if extendedEOT == 0 and trial['instructTrial'] == 0 and trial['catchTrial'] != 1:
-            return True
-        else:
-            return False
+    BD = (1/4*np.log(1/4*((v1**2/v2**2) + (v2**2/v1**2) +2))) + \
+         (1/4*(((m1-m2)**2)/(v1+v2)))
+    BC = np.exp(-BD)
+    
+    return BC
 
 
 def fieldInTrial(fieldList, trial = None):
@@ -234,11 +274,11 @@ def randTuningCurve(numNeurons):
                                    a dictionary
     '''
     tuningMat = np.zeros((numNeurons + 1, 6))
-    tuningMat[0] = np.arange(0,360,60)
+    tuningMat[0] = np.arange(360,720,60)
     tcDictionary = {}
 
     for i in range(1, tuningMat.shape[0]):
-        np.random.seed(2)
+        np.random.seed(i)
         amp = np.random.randint(15,30)
         y_translate = np.random.randint(30,50)
         x_translate = np.random.randint(60,120)
